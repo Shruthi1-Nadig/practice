cb4fa1bc1aa6637d528015fdb2a5731cfd202041
Shruthi Nadig <snadig@.visa.com>
2019-06-18 12:58:53 -0700

    relect example

--- reflect.go
+++ reflect.go
@@ -8,4 +8,6 @@ func main() {
 var i int32 = 213453
 fmt.Println("type of i", reflect.TypeOf(i))
 fmt.Println("value of i", reflect.ValueOf(i))
+fmt.Println("value of i", reflect.ValueOf(i).Type())
+fmt.Println(" of i", reflect.ValueOf(i).Kind())
 }
