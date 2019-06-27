7fa1ec2a062c623156786002cec43b16d5f7c8ee
Shruthi Nadig <snadig@.visa.com>
2019-06-27 14:55:44 -0700

    init func in lib package

---
+++ lib/map_init.go
@@ -0,0 +1,12 @@
+package lib
+
+func init() {
+var snacks map[string]string
+snacks = make(map[string]string)
+snacks["chips"] = "potato chips"
+snacks["cookies"] = "oreo"
+snacks["juice"] = "orange"
+snacks["candies"] = "mandm"
+}
+
+
